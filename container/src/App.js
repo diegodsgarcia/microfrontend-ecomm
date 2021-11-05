06431c82e704613bab166898c39145c67eb9ee8d
diegodsgarcia <diego.ds.garcia@gmail.com>
2021-11-05 18:44:08 -0300

    add marketing in container

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import MarketingApp from './components/MarketingApp'
+
 export default () => {
-  return <h1>hi there</h1>
+  return (
+    <>
+      <h1>hi there</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  )
 }
